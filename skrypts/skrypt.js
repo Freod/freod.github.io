6930410b301a10fb8386009fd5a1fb5b511b5e3e
Freod <radek.paradecki@gmail.com>
2018-09-24 19:13:28 +0200

    2018 - 05

--- skrypts/skrypt.js
+++ skrypts/skrypt.js
@@ -74,7 +74,7 @@
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			var myArr = JSON.parse(this.responseText);
-			//console.log(myArr);
+			console.log(myArr);
 			wyswietlanie(myArr);
 		}
 	};
